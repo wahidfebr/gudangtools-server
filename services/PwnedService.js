0d5557ff9ab91baa14cefdf0cc50652d50f86b0b
wahidfebr <wahidfebr@gmail.com>
2023-04-05 03:27:48 +0700

    fix: client only send prefix for security reason
    note: sha1 is not safe

--- services/PwnedService.js
+++ services/PwnedService.js
@@ -1,28 +1,14 @@
 const axios = require("axios");
 const baseUrl = "https://api.pwnedpasswords.com";
 
-const isPasswordPwned = async (suffix, prefix) => {
+const isPasswordPwned = async (prefix) => {
     try {
-        // * save result to isPwned
-        const isPwned = {
-            status: false,
-            count: 0
-        }
+        if (!prefix || prefix.length !== 5) throw { name: "BadRequest" };
 
         // * request data from pwnedpasswords
         const { data } = await axios.get(baseUrl + "/range/" + prefix.toUpperCase());
 
-        // * loop through data response string to check suffix and count
-        data.split("\n").find(el => {
-            const [dataHash, count] = el.split(':');
-            if (dataHash.toUpperCase() === suffix.toUpperCase()) {
-                isPwned.status = true;
-                isPwned.count = +count;
-                return isPwned
-            }
-        })
-
-        return isPwned;
+        return data;
     } catch (error) {
         error.SERVICES_ERROR = "PwnedService: isPasswordPwned";
         throw error;
