d598ee0f3aeeaa0a95209081edb479934afee6d4
wahidfebr <wahidfebr@gmail.com>
2023-04-05 12:55:33 +0700

    feat: add feature read and create assets

--- controllers/AssetController.js
+++ controllers/AssetController.js
@@ -1,8 +1,26 @@
-const { Asset } = require("../models");
+const { Asset, Member, Stock } = require("../models");
 
 class AssetController {
     static async readAll(req, res, next) {
         try {
+            const { id: MemberId } = req.member;
+
+            const assets = await Asset.findAll({
+                where: { MemberId },
+                include: [
+                    {
+                        model: Member,
+                        attributes: {
+                            exclude: ["password"]
+                        }
+                    },
+                    {
+                        model: Stock,
+                    },
+                ]
+            })
+
+            res.status(200).json(assets);
         } catch (error) {
             error.ERROR_FROM_FUNCTION = "AssetController: readAll";
             next(error);
@@ -11,6 +29,35 @@ class AssetController {
 
     static async create(req, res, next) {
         try {
+            const { id: MemberId } = req.member;
+            const {
+                id: StockId,
+                shares,
+                initialPrice
+            } = req.body;
+
+            if (!StockId ||
+                !shares ||
+                !initialPrice) {
+                throw { name: "BadRequest" };
+            }
+
+            const stock = await Stock.findByPk(StockId);
+            if (!stock) throw { name: "StockNotFound" };
+
+            const [asset, created] = await Asset.findOrCreate({
+                where: { StockId, MemberId },
+                defaults: {
+                    StockId,
+                    MemberId,
+                    shares,
+                    initialPrice
+                }
+            })
+
+            if (!created) throw { name: "DuplicateItem" };
+
+            res.status(201).json(asset);
         } catch (error) {
             error.ERROR_FROM_FUNCTION = "AssetController: create";
             next(error);
