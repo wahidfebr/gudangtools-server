7a925f4ddfa3fa95ac0da7a72162102aa22b623b
wahidfebr <wahidfebr@gmail.com>
2023-04-05 13:23:43 +0700

    feat: export service get stock price

--- services/index.js
+++ services/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     isPasswordPwned: require("./PwnedService").isPasswordPwned,
     fetchCompanies: require("./GoApiService").fetchCompanies,
+    getStockPriceByTickers: require("./GoApiService").getStockPriceByTickers,
 }
